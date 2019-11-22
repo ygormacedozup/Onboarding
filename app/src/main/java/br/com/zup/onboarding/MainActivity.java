e8356ab75b1fd459580dfd89073de4f3e0f3eaae
caiquesilvazup <caique.silva@zup.com.br>
2019-11-22 16:20:30 -0300

    Initial commit

---
+++ app/src/main/java/br/com/zup/onboarding/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.zup.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
