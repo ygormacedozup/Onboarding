c5822faa0aed23e1815e8a8e53235dfa9ed94ab3
Ygor.Macedo <ygormacedo44@gmail.com>
2019-11-25 18:50:35 -0300

    Dashoboarding XML

--- app/src/main/java/br/com/zup/onboarding/LoginActivity.java
+++ app/src/main/java/br/com/zup/onboarding/LoginActivity.java
@@ -2,15 +2,32 @@ package br.com.zup.onboarding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        nextView();
 
-        // Test for commit in project
+
+    }
+
+    private void nextView(){
+
+        ImageButton imgBtn = findViewById(R.id.ac_login_gmail);
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
