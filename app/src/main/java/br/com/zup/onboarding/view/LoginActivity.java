18bbdfa703d255412ed08a0e161516ad1a9ddb96
Ygor.Macedo <ygormacedo44@gmail.com>
2019-11-27 16:23:07 -0300

    sei la

--- app/src/main/java/br/com/zup/onboarding/view/LoginActivity.java
+++ app/src/main/java/br/com/zup/onboarding/view/LoginActivity.java
@@ -4,32 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
 import br.com.zup.onboarding.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "AndroidClarified";
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        nextView();
 
+        SignInButton googleSignInButton = findViewById(R.id.ac_login_gmail);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-    }
-
-    private void nextView(){
-
-        ImageButton imgBtn = findViewById(R.id.ac_login_gmail);
-        imgBtn.setOnClickListener(new View.OnClickListener() {
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                startActivity(intent);
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 101);
             }
         });
+
     }
 }
