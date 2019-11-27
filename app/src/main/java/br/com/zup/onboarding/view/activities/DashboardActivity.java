ecf198bb4fd837aea1cce66f6bee692b994a2d7f
caiquesilvazup <caique.silva@zup.com.br>
2019-11-27 16:25:27 -0300

    Alteração de packages

--- app/src/main/java/br/com/zup/onboarding/view/activities/DashboardActivity.java
+++ app/src/main/java/br/com/zup/onboarding/view/activities/DashboardActivity.java
@@ -1,4 +1,4 @@
-package br.com.zup.onboarding.view;
+package br.com.zup.onboarding.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/br/com/zup/onboarding/view/fragments/ResultFragment.java
+++ app/src/main/java/br/com/zup/onboarding/view/fragments/ResultFragment.java
@@ -1,4 +1,4 @@
-package br.com.zup.onboarding.view;
+package br.com.zup.onboarding.view.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
