ea379fb5bd05b35b70092f104d0cf8eb16cacb8a
caiquesilvazup <caique.silva@zup.com.br>
2019-11-27 10:10:13 -0300

    Melhorias no layout

--- app/src/main/java/br/com/zup/onboarding/view/QuestionFragment.java
+++ app/src/main/java/br/com/zup/onboarding/view/QuestionFragment.java
@@ -15,9 +15,11 @@ import br.com.zup.onboarding.R;
 import br.com.zup.onboarding.model.Question;
 
 public class QuestionFragment extends Fragment {
+    private int questionIndex;
     private Question question;
     private final String CORRECT_ANSWER_TAG = "CORRECT";
     private View rootView;
+    private ChangeFragmentListener listener;
     TextView questionNumber;
     TextView questionName;
     Button btnFirstAnswer;
@@ -25,8 +27,10 @@ public class QuestionFragment extends Fragment {
     Button btnThirdAnswer;
     Button btnFourthAnswer;
 
-    public QuestionFragment(Question question) {
+    public QuestionFragment(int questionIndex, Question question, ChangeFragmentListener listener) {
+        this.questionIndex = questionIndex;
         this.question = question;
+        this.listener = listener;
     }
 
     @Nullable
@@ -37,6 +41,8 @@ public class QuestionFragment extends Fragment {
 
         setLayout();
         setCorrectAnswerTag();
+        setAnswerBackground();
+        setCorrectAnswerClickListener();
 
         return view;
     }
@@ -49,6 +55,14 @@ public class QuestionFragment extends Fragment {
         btnSecondAnswer = rootView.findViewById(R.id.btn_second_answer);
         btnThirdAnswer = rootView.findViewById(R.id.btn_third_answer);
         btnFourthAnswer = rootView.findViewById(R.id.btn_fourth_answer);
+
+        questionNumber.setText("Questão " + questionIndex + ":");
+        questionName.setText(question.getQuestion());
+
+        btnFirstAnswer.setText(question.getAnswers().get(0));
+        btnSecondAnswer.setText(question.getAnswers().get(1));
+        btnThirdAnswer.setText(question.getAnswers().get(2));
+        btnFourthAnswer.setText(question.getAnswers().get(3));
     }
 
     private void setCorrectAnswerTag() {
@@ -70,4 +84,30 @@ public class QuestionFragment extends Fragment {
         }
     }
 
+    private void setAnswerBackground() {
+        int btnFirstAnswerBackground = (btnFirstAnswer.getTag() != null) ? R.drawable.btn_correct_answer_selector : R.drawable.btn_incorrect_answer_selector;
+        btnFirstAnswer.setBackgroundResource(btnFirstAnswerBackground);
+
+        int btnSecondAnswerBackground = (btnSecondAnswer.getTag() != null) ? R.drawable.btn_correct_answer_selector : R.drawable.btn_incorrect_answer_selector;
+        btnSecondAnswer.setBackgroundResource(btnSecondAnswerBackground);
+
+        int btnThirdAnswerBackground = (btnThirdAnswer.getTag() != null) ? R.drawable.btn_correct_answer_selector : R.drawable.btn_incorrect_answer_selector;
+        btnThirdAnswer.setBackgroundResource(btnThirdAnswerBackground);
+
+        int btnFourthAnswerBackground = (btnFourthAnswer.getTag() != null) ? R.drawable.btn_correct_answer_selector : R.drawable.btn_incorrect_answer_selector;
+        btnFourthAnswer.setBackgroundResource(btnFourthAnswerBackground);
+    }
+
+    private void setCorrectAnswerClickListener() {
+        rootView.findViewWithTag(CORRECT_ANSWER_TAG).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.changeFragment();
+            }
+        });
+    }
+
+    interface ChangeFragmentListener {
+        void changeFragment();
+    }
 }
