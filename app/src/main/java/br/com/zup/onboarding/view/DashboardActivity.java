0cf77bc56b0ed949051fa676a796efefe680ac6b
Ygor.Macedo <ygormacedo44@gmail.com>
2019-12-04 20:21:41 -0300

    New Branch

--- app/src/main/java/br/com/zup/onboarding/view/DashboardActivity.java
+++ app/src/main/java/br/com/zup/onboarding/view/DashboardActivity.java
@@ -1,32 +1,76 @@
 package br.com.zup.onboarding.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import br.com.zup.onboarding.R;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        startQuestions();
-    }
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-    private void startQuestions(){
-        Button btnNext = findViewById(R.id.db_continue_button);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        ImageView photoZupper = findViewById(R.id.db_logo_foguete);
+        TextView nameZuuper = findViewById(R.id.db_txt_name_receive);
+        TextView emailZuuper = findViewById(R.id.db_email_receive);
+        ImageButton btnBack = findViewById(R.id.back_btn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this,QuestionActivity.class);
-                startActivity(intent);
+                if (v.getId() == R.id.back_btn) {
+                    signOut();
+                }
             }
         });
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            nameZuuper.setText(personName);
+            emailZuuper.setText(personEmail);
+
+            Glide.with(this).load(personPhoto).circleCrop().into(photoZupper);
+
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        Toast.makeText(DashboardActivity.this, "Saindo do App", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
--- app/src/main/java/br/com/zup/onboarding/view/LoginActivity.java
+++ app/src/main/java/br/com/zup/onboarding/view/LoginActivity.java
@@ -1,12 +1,12 @@
 package br.com.zup.onboarding.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -16,34 +16,58 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
-
 import br.com.zup.onboarding.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String TAG = "AndroidClarified";
-    private GoogleSignInClient googleSignInClient;
+    private GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        SignInButton googleSignInButton = findViewById(R.id.ac_login_gmail);
+        SignInButton signInButton = findViewById(R.id.ac_login_gmail);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.ac_login_gmail) {
+                    signIn();
+                }
+            }
+        });
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
-        googleSignInClient = GoogleSignIn.getClient(this,gso);
-        googleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signInIntent = googleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, 101);
-            }
-        });
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+            startActivity(intent);
+
+        } catch (ApiException e) {
+            Log.w("Erro", "Ao tentar logar, tente novamente." + e.getStatusCode());
+        }
     }
 }
