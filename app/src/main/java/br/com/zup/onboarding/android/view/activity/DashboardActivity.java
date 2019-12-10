96b6545534fec0227c2343857ba08fa3599a0ea2
Ygor.Macedo <ygormacedo44@gmail.com>
2019-12-10 15:05:35 -0300

    TUTU SUBIU

--- app/src/main/java/br/com/zup/onboarding/android/view/activity/DashboardActivity.java
+++ app/src/main/java/br/com/zup/onboarding/android/view/activity/DashboardActivity.java
@@ -82,7 +82,7 @@ public class DashboardActivity extends AppCompatActivity {
         seeLater.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, ContentActivity.class);
+                Intent intent = new Intent(DashboardActivity.this, QuestionActivity.class);
                 startActivity(intent);
             }
         });
