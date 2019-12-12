420bff3434d36a39633e37b2613dc1dbe0e771e3
caiquesilvazup <caique.silva@zup.com.br>
2019-12-12 10:48:55 -0300

    Refatoração do login

---
+++ app/src/main/java/br/com/zup/onboarding/android/presenter/LoginPresenter.java
@@ -0,0 +1,54 @@
+package br.com.zup.onboarding.android.presenter;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import br.com.zup.onboarding.android.GoogleAuthentication;
+import br.com.zup.onboarding.android.contract.LoginContract;
+
+public class LoginPresenter implements LoginContract.Presenter {
+    private LoginContract.View view;
+    private GoogleAuthentication authentication;
+
+    public LoginPresenter(Activity activity) {
+        authentication = new GoogleAuthentication(activity);
+    }
+
+    @Override
+    public void start(LoginContract.View view) {
+        this.view = view;
+        view.setLoginButtonClickListener();
+    }
+
+    @Override
+    public void stop() {
+        view = null;
+    }
+
+    @Override
+    public void onSignInClicked() {
+        view.startSignInIntent();
+    }
+
+    @Override
+    public Intent getSignInIntent() {
+        return authentication.getSignInIntent();
+    }
+
+    @Override
+    public void onSignInResultReceived(Intent data) {
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            view.navigateToHome(account);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
